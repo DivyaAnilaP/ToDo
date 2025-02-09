22efa319ce218182ec673ee24b4a8b0cd3ac4058
DivyaAnila <pantangidivyaanila@gmail.com>
2025-02-09 13:33:22 +0530

    Updated API URL for live backend

--- my-app/src/api/todoApi.js
+++ my-app/src/api/todoApi.js
@@ -1,6 +1,8 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'http://localhost:8080/todos';
+
+const API_BASE_URL = "https://todoweb-hj4k.onrender.com";
+
 export const fetchTodos=() =>axios.get(API_BASE_URL);
 export const createTodo=(todo)=>axios.post(API_BASE_URL,todo);
 export const updateTodo =(id,todo)=>axios.put(`${API_BASE_URL}/${id}`,todo);
